c9cd8cf8ee7fe63023013a603909f7bbcb8c4fc6
RealBirdMan91 <birdman.coding.1991@gmail.com>
2024-12-11 15:19:55 +0100

    setup ts config

--- assets/svelte/Rules.svelte
+++ assets/svelte/Rules.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import { fly } from "svelte/transition";
     import type { Live } from "./types/live";
-    import { test } from "./utils/test";
+    import { test } from "./lib/test";
+    import { cn } from "./lib/utils";
 
     const myTest = test("testitest");
     console.log(myTest);
@@ -21,7 +22,9 @@
         <div in:fly={{ y: 50, duration: 300 }}>{emojis}</div>
     {/if}
     <div>
-        <h1 class="text-green-500 text-2xl">Rules</h1>
+        <h1 class={cn(visible ? "text-green-500" : "text-red-500", "text-2xl")}>
+            Rules
+        </h1>
         <p>Here are the rules:</p>
         <ul>
             <li>Rule 1</li>
