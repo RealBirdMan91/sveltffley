4878784eeb9f3843588abfbabb83cec8ea29885e
RealBirdMan91 <birdman.coding.1991@gmail.com>
2024-12-11 14:31:15 +0100

    handle esLint

--- config/config.exs
+++ config/config.exs
@@ -31,15 +31,7 @@ config :sveltffley, SveltffleyWeb.Endpoint,
 # at the `config/runtime.exs`.
 config :sveltffley, Sveltffley.Mailer, adapter: Swoosh.Adapters.Local
 
-# Configure esbuild (the version is required)
-config :esbuild,
-  version: "0.17.11",
-  sveltffley: [
-    args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
-    cd: Path.expand("../assets", __DIR__),
-    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
-  ]
+
 
 # Configure tailwind (the version is required)
 config :tailwind,
