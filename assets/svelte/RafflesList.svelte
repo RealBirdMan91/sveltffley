7f5b1febc5eb446a6b17d73b1916aca00be0488a
RealBirdMan91 <birdman.coding.1991@gmail.com>
2024-12-30 23:27:30 +0100

    fixed filtering

--- assets/svelte/RafflesList.svelte
+++ assets/svelte/RafflesList.svelte
@@ -1,25 +1,36 @@
 <script lang="ts">
-    import { type Raffle } from "$lib/types/raffle";
-    import RaffleCard from "$lib/components/shared/raffle/RaffleCard.svelte";
-    import FilterForm, {
-        type FormState,
-    } from "$lib/components/raffles/FilterForm.svelte";
-    import type { Live } from "live_svelte";
-    let { raffles, live }: { raffles: Raffle[]; live: Live } = $props();
+  import { type Raffle } from "$lib/types/raffle";
+  import RaffleCard from "$lib/components/shared/raffle/RaffleCard.svelte";
+  import FilterForm from "$lib/components/raffles/FilterForm.svelte";
+  import type { Live } from "live_svelte";
+  export type Params = {
+    query: string;
+    status: "upcoming" | "open" | "closed" | "";
+    sort_by: "prize" | "ticket_price" | "";
+  };
 
-    function onFilterChange(formState: FormState) {
-        live.pushEvent("filter", formState);
-    }
+  type Props = {
+    raffles: Raffle[];
+    live: Live;
+    params: Params;
+  };
+
+  let { raffles, live, params }: Props = $props();
 </script>
 
 <section class="container mx-auto p-4 flex flex-col space-y-6">
-    <h1 class="text-4xl text-neutral-800">Raffles</h1>
-    <FilterForm onChange={(values) => onFilterChange(values)} />
-    <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {#each raffles as raffle}
-            <li>
-                <RaffleCard {raffle} />
-            </li>
-        {/each}
-    </ul>
+  <h1 class="text-4xl text-neutral-800">Raffles</h1>
+  <FilterForm
+    values={params}
+    onFilterChange={(val) => {
+      live.pushEvent("filter", val);
+    }}
+  />
+  <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    {#each raffles as raffle}
+      <li>
+        <RaffleCard {raffle} />
+      </li>
+    {/each}
+  </ul>
 </section>
--- assets/svelte/components/raffles/FilterForm.svelte
+++ assets/svelte/components/raffles/FilterForm.svelte
@@ -1,88 +1,70 @@
 <script lang="ts">
-    import { Input } from "$lib/components/ui/input/index";
-    import * as Select from "$lib/components/ui/select/index";
-    import type { Selected } from "bits-ui";
+  import { Input } from "$lib/components/ui/input/index";
+  import * as Select from "$lib/components/ui/select/index";
+  import type { Params } from "$lib/RafflesList.svelte";
 
-    export type FormState = {
-        query: string;
-        status: "upcoming" | "open" | "closed" | "";
-        sort_by: "prize" | "ticket_price" | "";
-    };
+  type Props = { values: Params; onFilterChange: (params: Params) => void };
+  type Status = { value: Params["status"]; label: string };
+  type SortBy = { value: Params["sort_by"]; label: string };
 
-    type StatusValue = {
-        value: FormState["status"];
-        label: string;
-    };
+  const status: Status[] = [
+    { value: "", label: "Select a status" },
+    { value: "upcoming", label: "Upcoming" },
+    { value: "open", label: "Open" },
+    { value: "closed", label: "Closed" },
+  ];
+  const sort_by: SortBy[] = [
+    { value: "", label: "Sort by" },
+    { value: "prize", label: "Prize" },
+    { value: "ticket_price", label: "Ticket Price" },
+  ];
 
-    type SortByValue = {
-        value: FormState["sort_by"];
-        label: string;
-    };
-
-    const status: Array<StatusValue> = [
-        { value: "", label: "Select a status" },
-        { value: "upcoming", label: "Upcoming" },
-        { value: "open", label: "Open" },
-        { value: "closed", label: "Closed" },
-    ];
-    const sort_by: Array<SortByValue> = [
-        { value: "", label: "Sort by" },
-        { value: "prize", label: "Prize" },
-        { value: "ticket_price", label: "Ticket Price" },
-    ];
-
-    const { onChange } = $props<{
-        onChange: (formState: FormState) => void;
-    }>();
-
-    const formState = $state<FormState>({
-        query: "",
-        status: "",
-        sort_by: "",
-    });
-
-    $effect(() => {
-        onChange($state.snapshot(formState));
-    });
+  const { values, onFilterChange }: Props = $props();
 </script>
 
 <div class="flex gap-2">
-    <Input
-        bind:value={formState.query}
-        type="text"
-        placeholder="Search..."
-        autocomplete="off"
-        class="w-[280px]"
-    />
-    <Select.Root
-        selected={status.find((x) => x.value === formState.status) ?? status[0]}
-        onSelectedChange={(s: Selected<FormState["status"]> | undefined) => {
-            s && (formState.status = s.value);
-        }}
-    >
-        <Select.Trigger class="w-[180px]">
-            <Select.Value />
-        </Select.Trigger>
-        <Select.Content>
-            {#each status as { value, label }}
-                <Select.Item {value}>{label}</Select.Item>
-            {/each}
-        </Select.Content>
-    </Select.Root>
-    <Select.Root
-        selected={sort_by.find((x) => x.value === formState.sort_by) ??
-            sort_by[0]}
-        onSelectedChange={(s: Selected<FormState["sort_by"]> | undefined) => {
-            s && (formState.sort_by = s.value);
-        }}
-    >
-        <Select.Trigger class="w-[180px]">
-            <Select.Value />
-        </Select.Trigger>
-        <Select.Content>
-            {#each sort_by as { value, label }}
-                <Select.Item {value}>{label}</Select.Item>
-            {/each}
-        </Select.Content>
-    </Select.Root>
+  <Input
+    value={values.query}
+    onkeyup={(e) =>
+      onFilterChange({
+        ...values,
+        query: (e.target as HTMLInputElement).value,
+      })}
+    type="text"
+    placeholder="Search..."
+    autocomplete="off"
+    class="w-[280px]"
+  />
+  <Select.Root
+    items={status}
+    selected={status.find((s) => s.value === values.status)}
+    onSelectedChange={(field) => {
+      field && onFilterChange({ ...values, status: field.value });
+    }}
+  >
+    <Select.Trigger class="w-[180px]">
+      <Select.Value />
+    </Select.Trigger>
+    <Select.Content>
+      {#each status as { value: v, label: l }}
+        <Select.Item value={v}>{l}</Select.Item>
+      {/each}
+    </Select.Content>
+  </Select.Root>
+  <Select.Root
+    items={sort_by}
+    selected={sort_by.find((s) => s.value === values.sort_by)}
+    onSelectedChange={(field) => {
+      field && onFilterChange({ ...values, sort_by: field.value });
+    }}
+  >
+    <Select.Trigger class="w-[180px]">
+      <Select.Value />
+    </Select.Trigger>
+    <Select.Content>
+      {#each sort_by as { value: v, label: l }}
+        <Select.Item value={v}>{l}</Select.Item>
+      {/each}
+    </Select.Content>
+  </Select.Root>
 </div>
